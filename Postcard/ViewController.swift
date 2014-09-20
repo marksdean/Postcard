8bc66b009583ae17f360cb18d0a9a35c6c9e28a6
Mark Dean <mdean@mdeanphotographycom.local>
2014-09-19 21:13:02 -0700

    name label functionality

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,13 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
-    
     @IBOutlet weak var enterNameTextField: UITextField!
-    
     @IBOutlet weak var enterMessageTextField: UITextField!
-    
     @IBOutlet weak var mailButton: UIButton!
-    
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +25,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         // code will evaluate when we press the button
@@ -39,6 +35,10 @@ class ViewController: UIViewController {
         enterMessageTextField.resignFirstResponder()
         messageLabel.textColor = UIColor.redColor()
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
     }
 
 }
